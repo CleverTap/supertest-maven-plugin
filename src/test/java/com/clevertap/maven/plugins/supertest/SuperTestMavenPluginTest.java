d4849688cdb5d071d9ddd933119fc238aad0f019
Rishabh Arora <rishabharora1780@gmail.com>
2022-02-24 19:11:09 +0530

    resolving open threads. See #9

--- src/test/java/com/clevertap/maven/plugins/supertest/SuperTestMavenPluginTest.java
+++ src/test/java/com/clevertap/maven/plugins/supertest/SuperTestMavenPluginTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,46 +16,17 @@ import static junit.framework.Assert.assertEquals;
 
 class SuperTestMavenPluginTest {
     @Test
-    void createRerunCommandTest() throws IOException, ParserConfigurationException, SAXException  {
+    void createRerunCommandTest() throws IOException, ParserConfigurationException, SAXException, URISyntaxException {
         SuperTestMavenPlugin bv = new SuperTestMavenPlugin();
-        final String classXML1 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-                + "<testsuite xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"https://maven.apache.org/surefire/maven-surefire-plugin/xsd/surefire-test-report-3.0.xsd\" version=\"3.0\" name=\"com.example.FooTest\" time=\"0.353\" tests=\"0\" errors=\"0\" skipped=\"0\" failures=\"0\">\n"
-                + "<testcase name=\"\" classname=\"com.example.FooTest\" time=\"14.945\">\n"
-                + "<error message=\"Could not initialize class com.example.FooTest\" type=\"java.lang.NoClassDefFoundError\">\n"
-                + "at com.example.FooTest\n"
-                + "</error>\n"
-                + "</testcase>\n"
-                + "<testcase name=\"fooTest1\" classname=\"com.example.FooTest\" time=\"14.945\">\n"
-                + "<error message=\"Could not initialize class com.example.FooTest\" type=\"java.lang.NoClassDefFoundError\">\n"
-                + "at com.example.FooTest\n"
-                + "</error>\n"
-                + "</testcase>\n"
-                + "</testsuite>";
-        final String classXML2 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-                + "<testsuite xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"https://maven.apache.org/surefire/maven-surefire-plugin/xsd/surefire-test-report-3.0.xsd\" version=\"3.0\" name=\"com.example.BarTest\" time=\"0.353\" tests=\"0\" errors=\"0\" skipped=\"0\" failures=\"0\">\n"
-                + "<testcase name=\"barTest1\" classname=\"com.example.BarTest\" time=\"14.945\">\n"
-                + "<error message=\"Could not initialize class com.example.BarTest\" type=\"java.lang.NoClassDefFoundError\">]\n"
-                + "at com.example.BarTest\n"
-                + "</error>\n"
-                + "</testcase>\n"
-                + "<testcase name=\"barTest2\" classname=\"com.example.BarTest\" time=\"14.945\">\n"
-                + "<error message=\"Could not initialize class com.example.BarTest\" type=\"java.lang.NoClassDefFoundError\">\n"
-                + "at com.example.BarTest\n"
-                + "</error>\n"
-                + "</testcase>\n"
-                + "</testsuite>";
-
-        final Path xmlFile1 = Files.createTempFile("surefire", ".xml");
-        Files.write(xmlFile1, classXML1.getBytes(StandardCharsets.UTF_8));
-        final RunResult result1 = new SurefireReportParser(xmlFile1.toFile()).parse();
-
-        final Path xmlFile2 = Files.createTempFile("surefire", ".xml");
-        Files.write(xmlFile2, classXML2.getBytes(StandardCharsets.UTF_8));
-        final RunResult result2 = new SurefireReportParser(xmlFile2.toFile()).parse();
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL FooTest = classLoader.getResource("FooTest.xml");
+        URL BarTest = classLoader.getResource("BarTest.xml");
+        final RunResult FooTestResult = new SurefireReportParser(new File(FooTest.toURI())).parse();
+        final RunResult BarTestResult = new SurefireReportParser(new File(BarTest.toURI())).parse();
 
         final Map<String, List<String>> classnameToTestcaseList = new HashMap<>();
-        classnameToTestcaseList.put(result1.getClassName(), result1.getTestCases());
-        classnameToTestcaseList.put(result2.getClassName(), result2.getTestCases());
+        classnameToTestcaseList.put(FooTestResult.getClassName(), FooTestResult.getTestCases());
+        classnameToTestcaseList.put(BarTestResult.getClassName(), BarTestResult.getTestCases());
 
         String rerunCommand = bv.createRerunCommand(classnameToTestcaseList);
         System.out.println(rerunCommand);
